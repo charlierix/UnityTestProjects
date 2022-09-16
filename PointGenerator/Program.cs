816155dc412f47e020140f291dc383cf5e002f79
Charlie Rix <charlie.rix@gmail.com>
2022-09-16 10:25:18 -0500

    adding point generator

---
+++ PointGenerator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Vector3 from = Vector3.Zero;
+        Vector3 to = new Vector3(0, 0, -3);
+
+        int internal_points = 3;
+        float pow = 2;
+
+        Vector3[] points = GetPoints(from, to, internal_points, pow);
+
+        Console.WriteLine("unscaled points");
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Console.WriteLine(FormatPoint(points[i]));
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("half points");
+
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Vector3 half_point = points[i] + ((points[i + 1] - points[i]) / 2);
+            Console.WriteLine(FormatPoint(half_point));
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("distances");
+
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            float distance = (points[i + 1] - points[i]).Length();
+            Console.WriteLine(FormatScalar(distance));
+        }
+    }
+
+    private static Vector3[] GetPoints(Vector3 from, Vector3 to, int internal_points, float pow)
+    {
+        float[] x = GetPoints(from.X, to.X, internal_points, pow);
+        float[] y = GetPoints(from.Y, to.Y, internal_points, pow);
+        float[] z = GetPoints(from.Z, to.Z, internal_points, pow);
+
+        Vector3[] retVal = new Vector3[x.Length];
+
+        for (int i = 0; i < x.Length; i++)
+        {
+            retVal[i] = new Vector3(x[i], y[i], z[i]);
+        }
+
+        return retVal;
+    }
+    private static float[] GetPoints(float from, float to, int internal_points, float pow)
+    {
+        float[] retVal = new float[internal_points + 2];
+
+        float step = 1f / (retVal.Length - 1);
+        float gap = to - from;
+
+        for (int i = 0; i < retVal.Length; i++)
+        {
+            float percent = i * step;
+
+            retVal[i] = from + ((float)Math.Pow(percent, pow) * gap);
+            //retVal[i] = from + (percent * gap);
+        }
+
+        return retVal;
+    }
+
+    private static string FormatPoint(Vector3 point)
+    {
+        return $"{FormatScalar(point.X)}\t{FormatScalar(point.Y)}\t{FormatScalar(point.Z)}";
+    }
+    private static string FormatScalar(float value)
+    {
+        return Math.Round(value, 3).ToString();
+    }
+}
